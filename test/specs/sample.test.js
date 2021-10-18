8a4b6e9d5cf4d17de85a14bb9f0bec8c4f70dc89
Just-ish <92723054+Just-ish@users.noreply.github.com>
2021-10-18 12:27:58 +0200

    All these files are in the code
    All that's missing is the node modules that you can install via npm install :) 
    
    I'm not sure if these are the correct order :)

---
+++ test/specs/sample.test.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+const wdio = require("webdriverio");
+// const { inputUsername } = require('../pageobjects/login.page.js');
+// const LoginPage = require('../pageobjects/login.page.js')
+
+describe('Sample', () => {
+    it.only('Test', () => {
+        // $('~inputUsername').setValue("shamique");
+        // $('~inputPassword').setValue("123456");
+    })
+    //self.driver.quit()
+})
